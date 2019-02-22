2552326bdbddf463aeae030e90265fe983d9bb65
Laura SC <laura.shearer.cooper@gmail.com>
2019-02-21 21:41:57 -0700

    almost all functionality, and a screenshot to demonstrate

--- bamazon.js
+++ bamazon.js
@@ -18,43 +18,43 @@ var connection = mysql.createConnection({
 purchases();
 
 function purchases() {
-    connection.connect(function(err) {
-        if (err) throw err;
-              connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function (err, result) {
-                if (err) throw err;
-                console.log("Bamazon is open for business!");
-                console.log("Currently in stock:  ");
-                for (var i = 0; i < result.length; i++) {
-                  console.log("ID: " + result[i].item_id + " || Product: " + result[i].product_name + " || Price: $" + result[i].price + " || Quantity: " + result[i].stock_quantity);
-                }
-          
-              }); 
-         //  purchases();   
-      });
-      
-    inquirer
-      .prompt({
-        name: "action",
-        type: "list",
-        message: "Would you like to purchase something?",
-        choices: [
-          "Yes",
-          "Exit"
-        ]
-      })
-      .then(function(answer) {
-        switch (answer.action) {
+  connection.connect(function (err) {
+    if (err) throw err;
+    connection.query("SELECT `item_id`, `product_name`, `price`, `stock_quantity` FROM `products`", function (err, result) {
+      if (err) throw err;
+      console.log("\n Bamazon is open for business!");
+      console.log("\n Currently in stock:  ");
+      for (var i = 0; i < result.length; i++) {
+        console.log("ID: " + result[i].item_id + " || Product: " + result[i].product_name + " || Price: $" + result[i].price + " || Quantity: " + result[i].stock_quantity);
+      }
+
+    });
+    // purchases();   
+  });
+
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "Would you like to purchase something?",
+      choices: [
+        "Yes",
+        "Exit"
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.action) {
         case "Yes":
           selectItem();
           break;
-  
+
         case "Exit":
           connection.end();
           break;
-        }
-      });
-  }
-  
+      }
+    });
+}
+
 
 function selectItem() {
   inquirer
@@ -63,36 +63,51 @@ function selectItem() {
       type: "input",
       message: "Enter the ID number of the item you would like to purchase."
     })
-    .then(function(answer) {
-        var ID = answer;
-        inquirer
+    .then(function (itemAnswer) {
+      inquirer
         .prompt({
-            name: "quantity",
-            type: "input",
-            message: "Enter the quantity you would like to purchase."
-          })
-          .then(function(answer){
-            var quantity = answer;
-           // var query = 12;
-            console.log("You have purchased " + JSON.stringify(quantity) + " unit(s) of " + JSON.stringify(ID));
-            var sql = "UPDATE products SET stock_quantity = stock_quantity - " + quantity.propertyName +  "WHERE item_ID = " + ID.propertyName;
+          name: "quantity",
+          type: "input",
+          message: "Enter the quantity you would like to purchase."
+        })
+        .then(function (quantityAnswer) {
+
+          connection.query("SELECT product_name, stock_quantity FROM products WHERE item_ID = " + itemAnswer.itemID, function (err, result) {
+            if (err) throw err;
+            console.log(result);
+           // var idName = JSON.stringify(itemAnswer.itemID);
+            for (var i = 0; i < result.length; i++) {
+              // console.log("ID: " + result[i].item_id + " || Product: " + result[i].product_name + " || Price: $" + result[i].price + " || Quantity: " + result[i].stock_quantity);
+              var name = result[i].product_name;
+              var quantity = result[i].stock_quantity;
+              console.log("Item is:" + name + " Quantity is: " + quantity);
+            }
           
-                console.log("Updating database.....")
-              
+            if (quantity <= quantityAnswer.quantity) {
+              console.log("We do not have that many items in stock. Please try again.");
+              purchases();
+              return;
+            }   
+
+              console.log("You have purchased " + JSON.stringify(quantityAnswer.quantity) + " unit(s) of " + itemAnswer.itemID);
+              var sql = "UPDATE `products` SET `stock_quantity` = `stock_quantity` - " + JSON.stringify(quantityAnswer.quantity) + "WHERE `item_ID` = " + itemAnswer.itemID;
+              // console.log(sql);
+              console.log("Updating database.....")
+              purchases();
             connection.query(sql, function (err, result) {
                 if (err) throw err;
                 console.log(result.affectedRows + " record(s) updated");
-                purchases();
+                
               });
+                        
             });
             
-          });
+         
 
-        
-       
-      }
+        });
 
-    
- 
+    });
 
 
+
+}
